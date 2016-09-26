c35e66df205dbaa6f434d81db3a9a919bfdab984
dvdmon <levi@dvdmon.com>
2016-09-25 21:28:05 -0400

    Feat: added Ajax requestion to NYTimes API
    Created an Ajax request via the $.getJSON() jQuery method and used
    it to populate a section of the page with list items of article links
    based on the value of the City form field.

--- js/script.js
+++ js/script.js
@@ -21,7 +21,28 @@ function loadData() {
     $body.css("background-image", streetviewUrl);
 
 
-    // YOUR CODE GOES HERE!
+    // New York Times Ajax Request
+  //  var nytAPIUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  //  var nytKey = "f0420430c1ee4b5aa985bc1d977f8791";
+  //  var nytParams = "?apikey=" + apikey + "&q=" + city;
+  //  var nytFullUrl = nytAPIUrl + nytParams;
+    $.getJSON( "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=f0420430c1ee4b5aa985bc1d977f8791&q=" + city + "'&sort=newest" , function( data ) {
+        $("#nytimes-header").text('New York Times Articles About ' + city);
+
+         $.each( data.response.docs, function( key, val ) {
+            console.log(val);
+        $('<li class="article"><a href="' + val.web_url + '" target="_blank">' + val.headline.main + '</a><p>' + val.snippet + '</p></li>').appendTo($("#nytimes-articles"));
+         });
+
+
+    }).error(function() {
+        $("#nytimes-header").text('New York Times Articles Could Not Be Loaded. :(');
+
+    });
+
+
+
+
 
 
     return false;
